c2271e537706372497b52606325d74e4b453f87a
Yufan Sheng <syhily@gmail.com>
2023-04-28 17:21:58 +0800

    question: 18

---
+++ chapter3/question18_test.go
@@ -0,0 +1,32 @@
+package chapter3
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "is palindrome",
+			args: args{s: "Was it a cat I saw?"},
+			want: true,
+		},
+		{
+			name: "is not palindrome",
+			args: args{s: "race a car"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
