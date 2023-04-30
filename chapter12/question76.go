2b5bf71447cd99d464296090a1ce994836da5a27
Yufan Sheng <syhily@gmail.com>
2023-04-30 20:57:57 +0800

    question: 76, quick sort in golang.

--- chapter12/question76.go
+++ chapter12/question76.go
@@ -1,1 +1,35 @@
 package chapter12
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func quickSort(numbers ...int) []int {
+	innerQuickSort(numbers, 0, len(numbers)-1)
+	return numbers
+}
+
+func innerQuickSort(numbers []int, start, end int) {
+	if start >= end {
+		return
+	}
+
+	// Use a random value as the comparator.
+	random, _ := rand.Int(rand.Reader, big.NewInt(int64(end-start+1)))
+	pivot := int(random.Int64()) + start
+
+	numbers[start], numbers[pivot] = numbers[pivot], numbers[start]
+	pivot = start
+
+	for i := start + 1; i <= end; i++ {
+		if numbers[i] <= numbers[pivot] {
+			numbers[pivot], numbers[i] = numbers[i], numbers[pivot]
+			pivot++
+			numbers[pivot], numbers[i] = numbers[i], numbers[pivot]
+		}
+	}
+
+	innerQuickSort(numbers, start, pivot-1)
+	innerQuickSort(numbers, pivot+1, end)
+}
---
+++ chapter12/question76_test.go
@@ -0,0 +1,30 @@
+package chapter12
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_quickSort(t *testing.T) {
+	type args struct {
+		numbers []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "quick sort on an array",
+			args: args{numbers: []int{4, 1, 5, 6, 2, 7, 8, 3}},
+			want: []int{1, 2, 3, 4, 5, 6, 7, 8},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := quickSort(tt.args.numbers...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("quickSort() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
